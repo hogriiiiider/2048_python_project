9f9d600d34796e22707b2fb62697d81ba9b0f36e
unknown <ldongodongmo@gamingcampus.local>
2023-12-04 14:28:31 +0100

    ADD ajout d'un brouillon et modification d'un code

--- create_grid.py
+++ create_grid.py
@@ -1,36 +1,23 @@
-import random
-import create_grid as np
+import random 
 
-# Création d'un tableau 4x4 rempli de zéros
-tableau = np.zeros((4, 4), dtype=int)
+def grid_init():
+    grid = []
+    for row in range(4):
+        grid.append([''] * 4)
+    return grid
 
-# Fonction pour déplacer un point dans le tableau en évitant les bords
-def deplacer_point(position, direction):
-    x, y = position
+def grid_display(grid):
+    for row in grid:
+        print('  '.join(map(str, row)))
 
-    # Vérifier si le déplacement est autorisé
-    if (
-        (direction == "haut" and x > 0)
-        or (direction == "bas" and x < 3)
-        or (direction == "gauche" and y > 0)
-        or (direction == "droite" and y < 3)
-    ):
-        # Déplacer le point
-        if direction == "haut":
-            x -= 1
-        elif direction == "bas":
-            x += 1
-        elif direction == "gauche":
-            y -= 1
-        elif direction == "droite":
-            y += 1
+def get_number():
+    rand_num = random.randint(1, 10)
+    if rand_num <= 9:
+        return 2
+    else:
+        return 4
+    
+grid = grid_init()  # Correction ici : grid_init() au lieu de grid_init
 
-    return x, y
-
-# Exemple d'utilisation
-position_initiale = (2, 2)
-nouvelle_position = deplacer_point(position_initiale, "droite")
-
-# Afficher le tableau avec le point déplacé
-tableau[nouvelle_position] = 2
-print(tableau)
+for i in range(4):
+    grid_display(grid)
