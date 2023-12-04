ff8e74eacddc5b5c1c8b2f1a45c30ec2500fce4b
unknown <ldongodongmo@gamingcampus.local>
2023-12-04 14:34:57 +0100

    ADD ajout d'un brouillon et modification d'un code

--- brouillon_code.py
+++ brouillon_code.py
@@ -34,3 +34,9 @@ nouvelle_position = deplacer_point(position_initiale, "droite")
 # Afficher le tableau avec le point déplacé
 tableau[nouvelle_position] = 2
 print(tableau)
+
+#Gerer les limites du tableau(faut pas que les elements depassent la limite)
+#Gerer la position des elements du tableau(la position doit etre  aléatoire et doit etre compris entre (1,1) et  (4,4))
+#Gérer les collision entre éléments ( si on a le meme chiffres cote a cote on fusionne, sinon il restent à côté)
+#Gérer la situation ou il est impossibles d'ajouter les elements du tableau (vérifier l'impossibilité de fusionner les cases du tableau, /
+# /verifier que l'espace alloué soit bel et bien occupé dans sa totalité)
